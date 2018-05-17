9e6ddc256b823e0e1787b31c5e5fc94eeab1a67a
Turkyden <616287091@qq.com>
2018-05-18 00:01:46 +0800

    update module

---
+++ es6/fu-component-es6.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+
+class FuComponent {
+    constructor() {
+        // 默认配置
+        this.config = {
+            name: 'Mengmeng'
+        };
+        // 组件对象
+        this.conponent = '';
+        // 缓存
+        this.cache = {};
+        // 混合配置参数
+        //this.config = $.extend(this.config, option || {});
+    }
+    // 渲染-组件DOM
+    renderDom(dom) {
+        let tag =   `<div class="fu-component">
+                        <h1>Welcome to FunUI !</h1>
+                        <h2>Hi, i am a fu-component !</h2>
+                    </div>`;
+        dom.innerHTML = tag;        
+    }
+    // 公有方法-初始化
+    init() {
+        console.log(this._getConfig(this.config));
+    }
+    // 共有方法-打个招呼
+    sayHello(name) {
+        console.log(`hi i am not ${this.config.name} i am ${name}`);
+    }
+    // 私有方法-创建实例
+    _getConfig(config) {
+        return config;
+    }
+}
+
+export default FuComponent;
